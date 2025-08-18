7df274c7ce0a63625bda72d8ba350dd0277378aa
DESKTOP-QCHTQAR\god09 <god0937@gmail.com>
2025-08-18 15:23:32 +0900

    25-08-18 프로젝트 설정 중

---
+++ src/test/java/org/example/application_project/ApplicationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.application_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
